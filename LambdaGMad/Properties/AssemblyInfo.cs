67ced95300ee6476d2f9da4dba9679489d5845a6
LambdaGaming <starwars18m@gmail.com>
2020-10-28 19:06:30 -0400

    Updated screenshot and version number

--- LambdaGMad/Properties/AssemblyInfo.cs
+++ LambdaGMad/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LambdaGMad")]
-[assembly: AssemblyDescription("gmad with GUI, written in C#, with realtime access for Garry's Mod addon files")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Whisperity")]
-[assembly: AssemblyProduct("LambdaGMad")]
-[assembly: AssemblyCopyright("Copyright © 2013, Whisperity")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle( "LambdaGMad" )]
+[assembly: AssemblyDescription( "gmad with GUI, written in C#, with realtime access for Garry's Mod addon files" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "Whisperity" )]
+[assembly: AssemblyProduct( "LambdaGMad" )]
+[assembly: AssemblyCopyright( "Copyright © 2013, Whisperity" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7dab87ab-b8e7-4560-8e47-961d25d89ac1")]
+[assembly: Guid( "7dab87ab-b8e7-4560-8e47-961d25d89ac1" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2")]
-[assembly: AssemblyFileVersion("1.2")]
+[assembly: AssemblyVersion( "1.0" )]
+[assembly: AssemblyFileVersion( "1.0" )]
